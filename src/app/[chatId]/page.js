35017f448eabd82ad9aa357686d3907d97db090b
agorapilled <johngardner13@outlook.com>
2025-06-02 11:04:18 -0400

    v0

--- src/app/[chatId]/page.js
+++ src/app/[chatId]/page.js
@@ -66,7 +66,6 @@ export default function ChatPage() {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="text-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-black mx-auto mb-4"></div>
           <p className="text-gray-600">Loading chat...</p>
         </div>
       </div>
