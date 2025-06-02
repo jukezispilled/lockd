c9e69cbf14f350699e1061cb7c1965a76c7a7cff
agorapilled <johngardner13@outlook.com>
2025-06-02 08:38:21 -0400

    v0

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -41,7 +41,7 @@ export const MessageInput = ({ onSendMessage, disabled, chatId }) => {
   return (
     <div className="bg-white p-4">
       <form onSubmit={handleSubmit} className="flex items-end space-x-3">
-        <div className="flex-1 relative w-[75%]">
+        <div className="flex-1 relative">
           <textarea
             ref={textareaRef}
             value={message}
