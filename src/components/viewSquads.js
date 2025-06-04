0436b312f28afdd99038ce0ad31e67f6263db9f4
agorapilled <johngardner13@outlook.com>
2025-06-04 06:46:20 -0400

    v0

--- src/components/viewSquads.js
+++ src/components/viewSquads.js
@@ -167,7 +167,7 @@ export default function Squad() {
             >
                 <div>
                     <div className='absolute top-2 left-2 w-[65%]'>
-                        <p className="text-white text-2xl font-semibold text-wrap line-clamp-2">
+                        <p className="text-white text-2xl font-semibold break-words line-clamp-2">
                             {chat.name || "Untitled Group Chat"}
                         </p>
                         <p className="text-gray-400 text-base mt-[2px] line-clamp-2">
