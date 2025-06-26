4b6f27dc001c74d8cdd3ee868501b3dddc8ad0d1
agorapilled <johngardner13@outlook.com>
2025-06-26 02:36:24 -0400

    v0

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,23 @@ export default function Home() {
                     className="text-gray-700 text-lg font-semibold px-4 py-2 transition-all duration-300 border-0 cursor-pointer hover:scale-[102%] ease-in-out"
                   >
                     <div className="flex justify-center items-center gap-2">
-                      <span className="text-lg">{showTokenz ? 'NVM' : '[ Create + ]'}</span>
+                      {showTokenz ? (
+                        <span className="text-lg">NVM</span>
+                      ) : (
+                        <motion.span
+                          className="text-lg"
+                          animate={{
+                            scale: [1, 1.04, 1],
+                          }}
+                          transition={{
+                            duration: .7,
+                            repeat: Infinity,
+                            ease: "easeInOut"
+                          }}
+                        >
+                          [ Create + ]
+                        </motion.span>
+                      )}
                     </div>
                   </button>
                 </div>
