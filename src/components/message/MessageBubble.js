bb92803dfe4814f5859d087565363b40a7ca147f
agorapilled <johngardner13@outlook.com>
2025-06-28 20:35:39 -0400

    v0

--- src/components/message/MessageBubble.js
+++ src/components/message/MessageBubble.js
@@ -46,7 +46,7 @@ export const MessageBubble = ({ message, isOwn, showAvatar }) => {
         <div
           className={`px-4 py-2 rounded-2xl ${
             isOwn
-              ? 'bg-black text-white rounded-br-none'
+              ? 'bg-[#007aff] text-white rounded-br-none'
               : 'bg-white border border-gray-200 text-gray-800 rounded-bl-none'
           }`}
         >
