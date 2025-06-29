afa037677933af46c0595dbe3b1a09d95ffd579f
agorapilled <johngardner13@outlook.com>
2025-06-29 09:06:42 -0400

    v0

--- src/components/viewSquads.js
+++ src/components/viewSquads.js
@@ -206,7 +206,7 @@ export default function Squad({ refreshTrigger, isRefreshing }) {
 
   return (
     <div className='mt-[10%] md:mt-[7%]'>
-      <div className="text-4xl md:text-8xl font-bold text-center mb-1 text-white">Find Your Squad.</div>
+      <div className="text-4xl md:text-6xl font-bold text-center mb-1 text-white">Find Your Squad.</div>
       <div className="text-sm md:text-lg font-bold text-center mb-4 text-gray-300">time to get lockd in</div>
       
       {/* Search Bar */}
