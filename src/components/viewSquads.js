b8336e01d39e1280a336fdb91a73013e871cba27
agorapilled <johngardner13@outlook.com>
2025-06-29 17:47:03 -0400

    v0

--- src/components/viewSquads.js
+++ src/components/viewSquads.js
@@ -289,7 +289,7 @@ export default function Squad({ refreshTrigger, isRefreshing }) {
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.25 }}
-                className="h-[225px] w-full max-w-[400px] md:w-[225px] bg-black border border-black hover:border-[#333] transition duration-150 ease-in-out p-2 md:p-0 flex flex-col justify-between relative"
+                className="h-[225px] w-full max-w-[400px] md:w-[225px] bg-black border border-black hover:border-[#333] transition duration-100 ease-in-out p-2 md:p-0 flex flex-col justify-between relative"
                 onClick={() => handleChatClick(chat._id)}
               >
                 <div>
